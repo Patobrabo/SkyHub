3c7cb25f40f82ca7dde88f68e794857b1b661c5f
pato <90711587+Patobrabo@users.noreply.github.com>
2022-12-11 11:01:13 -0300

    Update Loading.lua

--- Loading.lua
+++ Loading.lua
@@ -8,14 +8,79 @@ MainSection:NewButton("Neva Hub", "", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2"))()
 end)
 
-MainSection:NewButton("HoHo Hub", function()
+MainSection:NewButton("HoHo Hub", "", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
 end)
 
-MainSection:NewButton("Zen Hub", function()
+MainSection:NewButton("Zen Hub", "", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader'))()
 end)
 
-MainSection:NewButton("Mokuro Hub", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader'))()
+MainSection:NewButton("Mokuro Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua'))()
+end)
+
+MainSection:NewButton("Mango Hub", "", function()
+    loadstring(game:HttpGet('https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta'))()
+end)
+
+MainSection:NewButton("Tawan Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt'))()
+end)
+
+MainSection:NewButton("Tuper Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TUPERX/TUPERX/Main/MenuBF/README.md'))()
+end)
+
+MainSection:NewButton("Unique Hub", "", function()
+    loadstring(game:HttpGet('https://rawscripts.net/raw/UPDATE-16-Blox-Fruits-Unique-Hub-LEAK-6735'))()
+end)
+
+MainSection:NewButton("BlazeX Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/WindowsXp12/BlxzeRBLX/main/BLxzeHub.lua'))()
+end)
+    
+MainSection:NewButton("2X Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NeaPchX2/2XHUB-UPDBIG/main/README.md'))()
+end)
+
+MainSection:NewButton("PlaybackX Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NeaPchX2/Playback-X-HUB/main/Protected.lua.txt'))()
+end)
+
+MainSection:NewButton("East Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Brineeee/EastHub/main/lua'))()
+end)
+
+MainSection:NewButton("Chiba Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KindIhave/ChibaHuB/main/Chiba-BF.txt'))()
+end)
+
+MainSection:NewButton("One x Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/xOne2/One-x-HUB/main/README.md'))()
+end)
+
+MainSection:NewButton("Putaya Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Putaya/HITBOXKAK/main/PutayaFree'))()
+end)
+
+MainSection:NewButton("Project Meow Hub", "", function()
+    loadstring(game:HttpGet('https://rawscripts.net/raw/Project-Meow_421'))()
+end)
+
+MainSection:NewButton("Fusion Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/SHAREHACK/script/main/fusion1'))()
+end)
+
+MainSection:NewButton("MasterX Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BlodyXHub/Xeniel_All/main/ForAll'))()
+end)
+
+local Credits = Window:NewTab("Credits")
+local CreditsSection = Credits:NewSection("Credits")
+
+CreditsSection:NewLabel("By: Pato (math193i)")
+
+CreditsSection:NewButton("Discord", "", function()
+    https://discord.io/Pato-Corporation
 end)
